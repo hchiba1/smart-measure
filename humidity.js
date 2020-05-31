4bf7b0c52f3576a4285998630ac658d10ca1ef63
Hirokazu Chiba <hchiba1@gmail.com>
2020-05-31 17:41:15 +0900

    add funciton to humidity.js

--- humidity.js
+++ humidity.js
@@ -5,13 +5,16 @@ const commander = require('commander')
       .option('-t, --temp <TEMP>', 'temperature', parseFloat)
       .parse(process.argv);
 
-const M = 18.01528;
-const R = 8.314;
+const vol_humid = get_vol_humid(commander.temp, commander.rel);
+console.log(vol_humid, 'g/m3');
 
-const t = commander.temp;
-const rel_humid = commander.rel;
-const eq_p = 6.1078 * 10 ** ((7.5 * t) / (t + 237.3));
+// Functions
+function get_vol_humid(t, rel_humid) {
+  const M = 18.01528;
+  const R = 8.314;
 
-const vol_humid = M/R * (eq_p * rel_humid) / (t + 273.15);
+  const eq_p = 6.1078 * 10 ** ((7.5 * t) / (t + 237.3));
+  const vol_humid = M/R * (eq_p * rel_humid) / (t + 273.15);
 
-console.log(vol_humid);
+  return vol_humid;
+}
