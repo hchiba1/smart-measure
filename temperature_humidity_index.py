94cdef2d673758c7e8e36d470fae8f97e4ed3f99
Hirokazu Chiba <hchiba1@gmail.com>
2020-08-20 05:29:37 +0900

    add messages and -v option

--- temperature_humidity_index.py
+++ temperature_humidity_index.py
@@ -4,19 +4,38 @@ import argparse
 parser = argparse.ArgumentParser(description='Calculate temperature-humidity index')
 parser.add_argument('-t', '--temperature', help='Celsius temperature')
 parser.add_argument('-r', '--humidity', help='relative humidity')
+parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
 args = parser.parse_args()
 
 if not args.temperature or not args.humidity:
     parser.print_help()
-    exit
+    exit(0)
 
-di = 46.3
 t = float(args.temperature)
 h = float(args.humidity)
+di = 0.81*t + 0.01*h*(0.99*t-14.3) + 46.3
 
-print(0.81*t)
-print(di + 0.81*t)
-print(0.99*t-14.3)
-print((0.99*t-14.3)*h)
-print((0.99*t-14.3)*h*0.01)
-print(di + 0.81*t + (0.99*t-14.3)*h*0.01)
+if args.verbose:
+    print('0.81T =', 0.81*t, sep="\t")
+    print('0.01H(0.99T-14.3) =', (0.99*t-14.3)*h*0.01)
+    # print('0.01H(T-14.3) =', (t-14.3)*h*0.01)
+    print('0.81T + 0.01H(0.99T-14.3) + 46.3 =', di)
+else:
+    print(di, end=': ')
+
+if di >= 85:
+    print('Too Hot!!')
+elif di >= 80:
+    print('Hot')
+elif di >= 77:
+    print('Bit Hot')
+elif di >= 70:
+    print('Not Hot')
+elif di >= 65:
+    print('Good!')
+elif di >= 60:
+    print('None')
+elif di >= 55:
+    print('Bit Cold')
+else:
+    print('Cold')
